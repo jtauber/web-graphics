bba130b2d7234858ef0b4c87605923cdbdbab1c2
Jannis Leidel <jannis@leidel.info>
2009-03-31 13:46:47 -0500

    Initial checkin

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='graphics',
+    version='0.1.0dev',
+    author='James Tauber',
+    url='http://github.com/pinax/graphics/',
+    py_modules=['gradient'],
+)
