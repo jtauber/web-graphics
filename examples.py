a0639c2b74a68d168367dbb81f12691d59dc671d
James Tauber <jtauber@jtauber.com>
2009-08-15 04:39:22 -0400

    refactored code

--- examples.py
+++ examples.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from graphics import write_png, gradient, radial_gradient
+from graphics import write_png, gradient, LINEAR, RADIAL
 
 ## EXAMPLES
 
@@ -8,31 +8,31 @@ from graphics import write_png, gradient, radial_gradient
 # so you can more easily see the result
 
 # body background from jtauber.com and quisition.com
-write_png("example1.png", 50, 143, gradient([
+write_png("example1.png", 50, 143, gradient(LINEAR, [
     (1.0, (0xA1, 0xA1, 0xA1), (0xDF, 0xDF, 0xDF)),
 ]))
 
 # header background similar to that on jtauber.com
-write_png("example2.png", 50, 90, gradient([
+write_png("example2.png", 50, 90, gradient(LINEAR, [
     (0.43, (0xBF, 0x94, 0xC0), (0x4C, 0x26, 0x4C)), # top
     (0.85, (0x4C, 0x26, 0x4C), (0x27, 0x13, 0x27)), # bottom
     (1.0,  (0x66, 0x66, 0x66), (0xFF, 0xFF, 0xFF)), # shadow
 ]))
 
 # original header gradient from pinax
-write_png("example3.png", 50, 80, gradient([
+write_png("example3.png", 50, 80, gradient(LINEAR, [
     (0.72, (0x00, 0x26, 0x4D), (0x00, 0x40, 0x80)),
     (1.0,  (0x00, 0x40, 0x80), (0x00, 0x6C, 0xCF)), # glow
 ]))
 
 # form input background from pinax
-write_png("example4.png", 50, 25, gradient([
+write_png("example4.png", 50, 25, gradient(LINEAR, [
     (0.33, (0xDD, 0xDD, 0xDD), (0xF3, 0xF3, 0xF3)), # top-shadow
     (1.0,  (0xF3, 0xF3, 0xF3), (0xF3, 0xF3, 0xF3)),
 ]))
 
 # current header gradient from pinax
-write_png("example5.png", 50, 80, gradient([
+write_png("example5.png", 50, 80, gradient(LINEAR, [
     (1.00, (0x00, 0x11, 0x33), (0x00, 0x55, 0x77)),
 ]))
 
@@ -41,7 +41,7 @@ write_png("example5.png", 50, 80, gradient([
 
 from colorsys import hsv_to_rgb
 
-write_png("example6.png", 200, 40, gradient([
+write_png("example6.png", 200, 40, gradient(LINEAR, [
     (0.5, hsv_to_rgb(0.55, 0.4, 122), hsv_to_rgb(0.55, 0.4, 161)),
     (1.0, hsv_to_rgb(0.55, 0.4, 143), hsv_to_rgb(0.55, 0.4, 175)),
 ]))
@@ -49,6 +49,6 @@ write_png("example6.png", 200, 40, gradient([
 
 # radial gradient
 
-write_png("example7.png", 400, 400, radial_gradient(0.5, 0.2, [
+write_png("example7.png", 400, 400, gradient(RADIAL(0.5, 0.2), [
     (1.0, (0xCC, 0xCC, 0xCC), (0x00, 0x00, 0x00)),
 ]))
--- graphics.py
+++ graphics.py
@@ -1,31 +1,30 @@
+import zlib
+import struct
+import array
+
+def output_chunk(out, chunk_type, data):
+    out.write(struct.pack("!I", len(data)))
+    out.write(chunk_type)
+    out.write(data)
+    checksum = zlib.crc32(data, zlib.crc32(chunk_type))
+    out.write(struct.pack("!I", checksum))
+
+def get_data(width, height, rgb_func):
+    fw = float(width)
+    fh = float(height)
+    compressor = zlib.compressobj()
+    data = array.array("B")
+    for y in range(height):
+        data.append(0)
+        fy = float(y)
+        for x in range(width):
+            fx = float(x)
+            data.extend([int(v * 255) for v in rgb_func(fx / fw, fy / fh)])
+    compressed = compressor.compress(data.tostring())
+    flushed = compressor.flush()
+    return compressed + flushed
+
 def write_png(filename, width, height, rgb_func):
-    
-    import zlib
-    import struct
-    import array
-    
-    def output_chunk(out, chunk_type, data):
-        out.write(struct.pack("!I", len(data)))
-        out.write(chunk_type)
-        out.write(data)
-        checksum = zlib.crc32(data, zlib.crc32(chunk_type))
-        out.write(struct.pack("!I", checksum))
-    
-    def get_data(width, height, rgb_func):
-        fw = float(width)
-        fh = float(height)
-        compressor = zlib.compressobj()
-        data = array.array("B")
-        for y in range(height):
-            data.append(0)
-            fy = float(y)
-            for x in range(width):
-                fx = float(x)
-                data.extend([int(v * 255) for v in rgb_func(fx / fw, fy / fh)])
-        compressed = compressor.compress(data.tostring())
-        flushed = compressor.flush()
-        return compressed + flushed
-    
     out = open(filename, "w")
     out.write(struct.pack("8B", 137, 80, 78, 71, 13, 10, 26, 10))
     output_chunk(out, "IHDR", struct.pack("!2I5B", width, height, 8, 2, 0, 0, 0))
@@ -33,32 +32,25 @@ def write_png(filename, width, height, rgb_func):
     output_chunk(out, "IEND", "")
     out.close()
 
+
 def linear_gradient(start_value, stop_value, start_offset=0.0, stop_offset=1.0):
     return lambda offset: (start_value + ((offset - start_offset) / (stop_offset - start_offset) * (stop_value - start_value))) / 255.0
 
-def gradient(DATA):
-    def gradient_function(x, y):
-        initial_offset = 0.0
-        for offset, start, end in DATA:
-            if y < offset:
-                r = linear_gradient(start[0], end[0], initial_offset, offset)(y)
-                g = linear_gradient(start[1], end[1], initial_offset, offset)(y)
-                b = linear_gradient(start[2], end[2], initial_offset, offset)(y)
-                return r, g, b
-            initial_offset = offset
-    return gradient_function
+def LINEAR(x, y):
+    return y
 
-def radial_gradient(center_x, center_y, DATA):
+def RADIAL(center_x, center_y):
+    return lambda x, y: (x - center_x) ** 2 + (y - center_y) ** 2
+
+def gradient(func, DATA):
     def gradient_function(x, y):
-        # distance from centerpoint
-        d = (x - center_x) ** 2 + (y - center_y) ** 2
-        
         initial_offset = 0.0
-        for offset, start, end, in DATA:
-            if d < offset:
-                r = linear_gradient(start[0], end[0], initial_offset, offset)(d)
-                g = linear_gradient(start[1], end[1], initial_offset, offset)(d)
-                b = linear_gradient(start[2], end[2], initial_offset, offset)(d)
+        f = func(x, y)
+        for offset, start, end in DATA:
+            if f < offset:
+                r = linear_gradient(start[0], end[0], initial_offset, offset)(f)
+                g = linear_gradient(start[1], end[1], initial_offset, offset)(f)
+                b = linear_gradient(start[2], end[2], initial_offset, offset)(f)
                 return r, g, b
             initial_offset = offset
     return gradient_function
