45531b45f2f26e0aeb3aabd1f329370815ab2d9c
James Tauber <jtauber@jtauber.com>
2010-02-24 15:42:35 -0700

    changed version and added description and author_email to setup

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ from setuptools import setup
 
 setup(
     name = "graphics",
-    version = "0.1.0dev",
+    version = "0.1",
+    description = "utilities for generating website graphics like gradients and textures",
     author = "James Tauber",
+    author_email = "jtauber@jtauber.com",
     url = "http://github.com/pinax/graphics/",
     py_modules = ["gradient"],
 )
