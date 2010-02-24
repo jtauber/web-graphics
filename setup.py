3c7fb43c42eee0db2c01978a54273e89f2439e65
James Tauber <jtauber@jtauber.com>
2010-02-24 17:45:06 -0500

    fixed name, url and py_modules

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name = "graphics",
+    name = "web-graphics",
     version = "0.1",
     description = "utilities for generating website graphics like gradients and textures",
     author = "James Tauber",
     author_email = "jtauber@jtauber.com",
-    url = "http://github.com/pinax/graphics/",
-    py_modules = ["gradient"],
+    url = "http://github.com/jtauber/web-graphics/",
+    py_modules = ["web_graphics"],
 )
