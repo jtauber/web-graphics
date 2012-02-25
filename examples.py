7e8ecaa95ea6ad874eb001386581256a6c3aa5bf
James Tauber <jtauber@jtauber.com>
2012-02-25 04:12:47 -0500

    proper vertical whitespace

--- examples.py
+++ examples.py
@@ -7,12 +7,12 @@ from web_graphics import write_png, gradient, LINEAR_X, LINEAR_Y, RADIAL, NO_NOI
 # normally you would make these with width=1 but below I've made them 50
 # so you can more easily see the result
 
-# body background from jtauber.com and quisition.com
+# body background from old jtauber.com and quisition.com
 write_png("example1.png", 50, 143, gradient(LINEAR_Y, NO_NOISE, [
     (1.0, (0xA1, 0xA1, 0xA1), (0xDF, 0xDF, 0xDF)),
 ]))
 
-# header background similar to that on jtauber.com
+# header background similar to that on old jtauber.com
 write_png("example2.png", 50, 90, gradient(LINEAR_Y, NO_NOISE, [
     (0.43, (0xBF, 0x94, 0xC0), (0x4C, 0x26, 0x4C)), # top
     (0.85, (0x4C, 0x26, 0x4C), (0x27, 0x13, 0x27)), # bottom
@@ -25,7 +25,7 @@ write_png("example3.png", 50, 80, gradient(LINEAR_Y, NO_NOISE, [
     (1.0,  (0x00, 0x40, 0x80), (0x00, 0x6C, 0xCF)), # glow
 ]))
 
-# form input background from pinax
+# old form input background from pinax
 write_png("example4.png", 50, 25, gradient(LINEAR_Y, NO_NOISE, [
     (0.33, (0xDD, 0xDD, 0xDD), (0xF3, 0xF3, 0xF3)), # top-shadow
     (1.0,  (0xF3, 0xF3, 0xF3), (0xF3, 0xF3, 0xF3)),
@@ -40,6 +40,7 @@ write_png("example5.png", 50, 80, gradient(LINEAR_Y, NO_NOISE, [
 write_png("example6.png", 50, 80, gradient(LINEAR_Y, NO_NOISE, [
     (0.5, (0x00, 0x11, 0x33), (0x00, 0x55, 0x77)),
 ]))
+
 
 # hsv example
 
@@ -69,6 +70,7 @@ write_png("example10.png", 400, 400, gradient(LINEAR_Y, GAUSSIAN(0.01), [
 write_png("example11.png", 960, 200, gradient(RADIAL(0.5, 0.0), GAUSSIAN(0.01), [
     (0.8, (0x22, 0x22, 0x22), (0x00, 0x00, 0x00)),
 ]))
+
 
 # horizontal gradient
 write_png("example12.png", 5000, 50, gradient(LINEAR_X, GAUSSIAN(0.005), [
