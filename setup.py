b1c3ec3e4494a360f0c7f69e73891a616c911a7e
James Tauber <jtauber@jtauber.com>
2014-07-15 19:07:01 -0400

    PEP8 compliance

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name = "web-graphics",
-    version = "0.1",
-    description = "utilities for generating website graphics like gradients and textures",
-    author = "James Tauber",
-    author_email = "jtauber@jtauber.com",
-    url = "http://github.com/jtauber/web-graphics/",
-    py_modules = ["web_graphics"],
+    name="web-graphics",
+    version="0.1",
+    description="utilities for generating website graphics like gradients and textures",
+    author="James Tauber",
+    author_email="jtauber@jtauber.com",
+    url="http://github.com/jtauber/web-graphics/",
+    py_modules=["web_graphics"],
 )
